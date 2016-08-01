3b924ce97088f2d8a8643f012f8cea0231afa31f
Johannes <jowettinger@gmail.com>
2016-08-01 19:46:51 +0200

    tab replaced by spaces

--- .scripts/generate.sh
+++ .scripts/generate.sh
@@ -2,7 +2,7 @@
 
 for NAME in all_node_types
 do
-	toscafy generate -c node_types -s node_types/$NAME.csarspec.json --refs-only -p -o generated/$NAME.csar
+  toscafy generate -c node_types -s node_types/$NAME.csarspec.json --refs-only -p -o generated/$NAME.csar
 done
 
 for NAME in ubuntu-on-vsphere
