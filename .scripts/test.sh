90acdf6064383a1429a7d0c7b1fefc75379c57ad
Johannes <jowettinger@gmail.com>
2016-08-25 13:50:39 +0200

    run smoketest CSAR in test stage

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -3,7 +3,6 @@
 git clone https://github.com/jojow/opentosca-dockerfiles.git dockerfiles
 
 cp generated/*.topology.csar dockerfiles/runner/csars/
-cp generated-playground/*.csar dockerfiles/runner/csars/
 
 cd dockerfiles
 
@@ -11,7 +10,7 @@ docker-compose -f docker-compose-runner.yml pull
 docker-compose -f docker-compose-runner.yml build
 docker-compose -f docker-compose-runner.yml up pre-runner
 
-export RUNNER_CONTEXT_DIR=$(pwd)/../playground/wordpress
+export RUNNER_CONTEXT_DIR=$(pwd)/../runners/smoketest
 docker-compose -f docker-compose-runner.yml up runner
 
 docker-compose -f docker-compose-runner.yml up post-runner
