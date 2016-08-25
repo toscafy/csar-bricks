24e695f10d34f7fdbf7d4549d11c60dadbd7e623
Johannes <jowettinger@gmail.com>
2016-08-25 14:12:33 +0200

    travis: parallel builds prepared

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -10,7 +10,7 @@ docker-compose -f docker-compose-runner.yml pull
 docker-compose -f docker-compose-runner.yml build
 docker-compose -f docker-compose-runner.yml up pre-runner
 
-export RUNNER_CONTEXT_DIR=$(pwd)/../runners/smoketest
+export RUNNER_CONTEXT_DIR=$(pwd)/../runners/$RUNNER
 docker-compose -f docker-compose-runner.yml up runner
 
 docker-compose -f docker-compose-runner.yml up post-runner
